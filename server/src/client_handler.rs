70b6f997e741d61c15d1beed65cbbe454fe0644a
David Dye <ethandye34@gmail.com>
2023-09-20 11:53:17 -0400

    print body

--- server/src/client_handler.rs
+++ server/src/client_handler.rs
@@ -1,28 +1,42 @@
-
-use std::{net::{ TcpStream, Shutdown, SocketAddr}, io::{BufReader, BufRead, Write}};
-
+use std::{
+    io::{self, BufRead, BufReader, Read, Write},
+    net::{Shutdown, SocketAddr, TcpStream},
+    str,
+};
 
 /**
  * function to handle a client
  */
 pub fn handle_client(stream: TcpStream, client_port: SocketAddr) {
-
-    
-    let reader = BufReader::new(&stream); // 2
-
-    for (index, line) in reader.lines().enumerate() { //Loop through all the lines in the file
-        eprintln!("{} - CURRENT LINE: {:?}", index, line);
+    let mut reader = BufReader::new(stream.try_clone().unwrap());
+    let mut name = String::new();
+    loop {
+        let r = reader.read_line(&mut name).unwrap();
+        if r < 3 {
+            //detect empty line
+            break;
+        }
     }
-
-    repond_client(client_port);
-
-    stream.shutdown(Shutdown::Both).expect("shutdown call failed");
-
+    let mut size = 0;
+    let linesplit = name.split("\n");
+    for l in linesplit {
+        if l.starts_with("Content-Length") {
+            let sizeplit = l.split(":");
+            for s in sizeplit {
+                if !(s.starts_with("Content-Length")) {
+                    size = s.trim().parse::<usize>().unwrap(); //Get Content-Length
+                }
+            }
+        }
+    }
+    let mut buffer = vec![0; size]; //New Vector with size of Content
+    reader.read_exact(&mut buffer).unwrap(); //Get the Body Content.
+    let body = str::from_utf8(&buffer).unwrap();
+    println!("{body}");
 }
 
 fn repond_client(client_addr: SocketAddr) {
     let stream = TcpStream::connect(client_addr);
-
 
     let _ = stream.unwrap().write(b"we good");
 }
--- server/src/main.rs
+++ server/src/main.rs
@@ -4,9 +4,7 @@ use utils::cmd_parser::parse_commands;
 mod client_handler;
 use std::net::TcpListener;
 
-
 fn main() -> Result<(), std::io::Error> {
-
     let arguments = parse_commands();
 
     match arguments.port_flag.as_str() {
@@ -14,19 +12,17 @@ fn main() -> Result<(), std::io::Error> {
             eprintln!("Listening on port: {}", arguments.port);
 
             server_loop(arguments.port);
-        },
+        }
         _ => println!("NOT VALID PORT FLAG!"),
     }
 
-   Ok(())        
-
+    Ok(())
 }
 
 /**
  * function start the server and begin listening
  */
 fn server_loop(port_string: i32) {
-
     /*
     starting off single threaded for now
     that means synchronously in rust right??
@@ -35,13 +31,12 @@ fn server_loop(port_string: i32) {
     let listener = TcpListener::bind(format!("127.0.0.1:{port_string}")).unwrap();
 
     let mut incoming = listener.incoming();
-    
+
     while let Some(stream) = incoming.next() {
         let s = stream.unwrap();
         let client_addr = s.peer_addr().unwrap();
         eprintln!("Accepting from: {:?}", s.peer_addr());
 
         let _ = client_handler::handle_client(s, client_addr);
-
     }
 }
